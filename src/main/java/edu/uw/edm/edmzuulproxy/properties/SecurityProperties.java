302821346c70e8d54b7303aaf323dbce684a4b95
Federico Silva Armas <790052+farmas@users.noreply.github.com>
2021-05-03 09:56:13 -0700

    CAB-4314: Adds ability to use x-audituser header for authentication and require it for docfinity routes. (#125)

--- src/main/java/edu/uw/edm/edmzuulproxy/properties/SecurityProperties.java
+++ src/main/java/edu/uw/edm/edmzuulproxy/properties/SecurityProperties.java
@@ -10,10 +10,16 @@ import lombok.Data;
 @ConfigurationProperties(prefix = "uw.profile.security")
 @Data
 public class SecurityProperties {
-
+    private static final String DOCFINIY_AUDIT_USER_HEADER = "x-audituser";
 
     private String keystoreLocation;
     private String keystorePassword;
     private String authenticationHeaderName;
 
+    /**
+     * Name of request header to use for authentication when routing to DocFinity server.
+     * 'X-AUDITUSER' by default, which is the header that DocFinity expects to use for auditing the
+     * actions performed by request.
+     */
+    private String docfinityAuthenticationHeaderName = DOCFINIY_AUDIT_USER_HEADER;
 }
